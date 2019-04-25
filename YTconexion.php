6e88b7ba817c17ed750f729b075250a4409db089
Hector Alejandro Sandoval Saucedo <alxsan@outlook.com>
2019-04-25 10:45:11 -0500

    Create YTconexion.php

---
+++ YTconexion.php
@@ -0,0 +1,27 @@
+<?php 
+
+	//iniciando la conexión
+	$conexion = new mysqli('localhost', 'root', '', 'psicosmart');
+	if($conexion->connect_errno){ //llamada a la función connect_errno por si hay error
+		echo 'fallo la conexión '. $conexión->connect_errno;
+	}
+
+	// mysqli_set_charset($conexion, 'utf8'); Forma procedimental
+	$conexion->set_charset('utf8'); //Forma POO para identificar carácteres latinos
+
+	$sql='SELECT * FROM users'; //haciendo la consulta 'SELECCIONA *TODO DESDE nombreTabla'
+
+
+	// $resultados = mysqli_query($conexion, $sql); Forma procedimental
+	$resultados=$conexion->query($sql); //Forma POO
+	if($conexion->errno){
+		die($conexion->error);
+	}
+
+	//while($fila=mysqli_fetch_array($resultados, MYSQL_ASSOC)){} Forma procedimental
+	while($fila=$resultados->fetch_assoc()){ //Forma POO
+		var_dump($fila);
+	}
+
+
+	$conexion->close();
