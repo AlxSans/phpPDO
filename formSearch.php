ac8af6736befb2f4c89b7ed3cdaf8a2262577a07
Hector Alejandro Sandoval Saucedo <alxsan@outlook.com>
2019-04-25 13:41:40 -0500

    Create formSearch.php
    Simple formulario para un ejemplo de búsqueda con PHP PDO POO SQL

---
+++ formSearch.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Search</title>
+</head>
+<body>
+
+<form action="searchPDO.php" method="POST">
+
+	<label>Search Email Owner:</label>
+	<input type="text" name="busqueda">
+	<input type="submit" name="submit" value="Search">
+</form>
+
+
+
+</body>
+</html>
